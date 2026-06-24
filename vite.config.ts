3b69a1624d3860ae15cebddd2f6f4b9962dbb123
guigui0246 <guigui0246@yahoo.com>
2026-06-24 18:22:04 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,44 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+function argPageRewrite() {
+  return {
+    name: 'arg-page-rewrite',
+    configureServer(server: import('vite').ViteDevServer) {
+      server.middlewares.use((request, _response, next) => {
+        if (!request.url) {
+          next();
+          return;
+        }
+
+        const url = new URL(request.url, 'http://localhost');
+
+        if (url.pathname === '/arg' || url.pathname === '/arg/') {
+          request.url = `/arg/index.html${url.search}`;
+        }
+
+        next();
+      });
+    },
+    configurePreviewServer(server: import('vite').PreviewServer) {
+      server.middlewares.use((request, _response, next) => {
+        if (!request.url) {
+          next();
+          return;
+        }
+
+        const url = new URL(request.url, 'http://localhost');
+
+        if (url.pathname === '/arg' || url.pathname === '/arg/') {
+          request.url = `/arg/index.html${url.search}`;
+        }
+
+        next();
+      });
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), argPageRewrite()],
 });
